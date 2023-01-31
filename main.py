99ec891dd248ed3dd309a69e10ad40df56783625
zaidsaid-007 <64166616+zaidsaid-007@users.noreply.github.com>
2023-01-31 16:02:18 +0300

    s

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
 # Beginning of 100 days of Python----Beginner---><---Advanced----
 # Start date January 31st 2023
+
+
+# + Day 1
+name = (input("Enter your name: \n"))
+print("Hello, ", name, "Welcome to Band name generator.")
+city = (input("Enter the name of your city: \n"))
+other = (input("Enter the name of anything you like or anything that sounds cool: \n"))
+
+print("Your Band name could be ", city + other)
